77531e760773790754d5d062b1d51cf28438b419
Toto <19580671+tomso11@users.noreply.github.com>
2023-04-15 00:04:56 -0300

    add image generator

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,14 @@
 import memesData from "../memesData";
+import React, { useState } from 'react';
 
 export const Meme = () => {
+    const [meme, setMeme] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+
     const handleClick = () => {
         // Generate a random index between 0 and the length of the memeData array
         const randomIndex = Math.floor(Math.random() * memesData.data.memes.length);
@@ -9,18 +17,22 @@ export const Meme = () => {
         const randomMeme = memesData.data.memes[randomIndex];
 
         // Return the URL property from the meme object
+        console.log(randomMeme);
         console.log(randomMeme.url);
+        setMeme(randomMeme.url);
+        
     }
 
     return(
         <div className="meme--container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="meme--form--inputs">
                     <input type="text" placeholder="Top text" className="meme-input"></input>
                     <input type="text" placeholder="Bottom text" className="meme-input"></input>
                 </div>
                 <button onClick={handleClick} type="submit" className="meme-button">Get a new meme image 🖼 </button>
             </form>
+            <img src={meme} className="meme--image" alt="Random Meme" />
         </div>
     )
 }
