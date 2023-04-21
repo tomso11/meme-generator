ee558b62f43ab97b469022a32afd254088afe8f7
Toto <19580671+tomso11@users.noreply.github.com>
2023-04-20 23:43:35 -0300

    added form inputs

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,24 +2,58 @@ import memesData from "../memesData";
 import React, { useState } from 'react';
 
 export const Meme = () => {
-    const [meme, setMeme] = useState("");
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        url: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [formData, setFormData] = useState({
+        topFormText: "",
+        bottomFormText: "",
+    });
+
+    const [allMemeImgs, setAllMemeImgs] = useState(memesData.data.memes);
+
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData =>{
+                    return ({
+                        ...prevFormData,
+                        [name] : value
+                    })
+                });
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        
+
+        setMeme(prevMeme => {
+            return ({
+                ...prevMeme,
+                topText: formData.topFormText,
+                bottomText: formData.bottomFormText
+                })}
+        );
     }
 
 
-    const handleClick = () => {
+    const getMemeImage = () => {
         // Generate a random index between 0 and the length of the memeData array
-        const randomIndex = Math.floor(Math.random() * memesData.data.memes.length);
+        const randomIndex = Math.floor(Math.random() * allMemeImgs.length);
 
         // Get the meme object from the memeData array using the random index
-        const randomMeme = memesData.data.memes[randomIndex];
+        const randomMeme = allMemeImgs[randomIndex];
 
         // Return the URL property from the meme object
         console.log(randomMeme);
         console.log(randomMeme.url);
-        setMeme(randomMeme.url);
+        setMeme(prevMeme => {
+            return ({
+                ...prevMeme,
+                url: randomMeme.url
+            })});
         
     }
 
@@ -27,12 +61,16 @@ export const Meme = () => {
         <div className="meme--container">
             <form onSubmit={handleSubmit}>
                 <div className="meme--form--inputs">
-                    <input type="text" placeholder="Top text" className="meme-input"></input>
-                    <input type="text" placeholder="Bottom text" className="meme-input"></input>
+                    <input type="text" placeholder="Top text" className="meme-input" name="topFormText" value={formData.topFormText} onChange={handleChange}></input>
+                    <input type="text" placeholder="Bottom text" className="meme-input" name="bottomFormText" value={formData.bottomFormText} onChange={handleChange}></input>
                 </div>
-                <button onClick={handleClick} type="submit" className="meme-button">Get a new meme image 🖼 </button>
+                <button onClick={getMemeImage} type="submit" className="meme-button">Get a new meme image 🖼 </button>
             </form>
-            <img src={meme} className="meme--image" alt="Random Meme" />
+            <div className="meme">
+                <img src={meme.url} className="meme--image" alt="Random Meme" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
